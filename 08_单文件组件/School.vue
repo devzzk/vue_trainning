3ffd6ba7f91d5a362f2dbd0aafd4fc6238ae152f
Donny Zhang <donny.zhang@mobilenowgroup.com>
2023-02-11 15:02:13 +0800

    单文件组件

---
+++ 08_单文件组件/School.vue
@@ -0,0 +1,36 @@
+<template>
+<!-- 组件的结构 -->
+    <div class="demo">
+        <h2>学校名称： {{ schooleName }}</h2>
+        <h2>学校地址： {{ schooleAddress }}</h2>
+    </div>
+</template>
+
+<script>
+    // js 代码
+    export default {
+        name: 'School',
+        data() {
+            return {
+                schooleName: '郑科院',
+                schooleAddress: '郑州'
+            }
+        },
+        methods: {
+            showName(){
+                alert(this.schooleName)
+            }
+        },
+    }
+
+</script>
+
+<style>
+/*
+组件的样式
+*/
+    .demo{
+        background-color: blue;
+    }
+
+</style>
---
+++ 08_单文件组件/Student.vue
@@ -0,0 +1,37 @@
+<template>
+<!-- 组件的结构 -->
+    <div class="demo">
+        <h2>学生姓名: {{ name }}</h2>
+        <h2>学生年龄: {{ age }}</h2>
+        <button @click="showName"> 提示学生姓名 </button>
+    </div>
+</template>
+
+<script>
+    // js 代码
+    export default {
+        name: 'Student',
+        data() {
+            return {
+                name: '郑科院',
+                age: 19
+            }
+        },
+        methods: {
+            showName(){
+                alert(this.name)
+            }
+        },
+    }
+
+</script>
+
+<style>
+/*
+组件的样式
+*/
+    .demo{
+        background-color: blue;
+    }
+
+</style>
